095d2c1583dd51e442ca3dc0253ef620176e4706
adrien <adrienbzr@outlook.com>
2026-08-18 20:58:41 -0300

    :sparkles: feat: adiciona resposta HTTP created no método post de Completudes

--- src/main/java/br/edu/ifce/retromarket/controllers/CompletudeController.java
+++ src/main/java/br/edu/ifce/retromarket/controllers/CompletudeController.java
@@ -3,11 +3,8 @@ package br.edu.ifce.retromarket.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import br.edu.ifce.retromarket.entities.Completude;
 import br.edu.ifce.retromarket.repositories.CompletudeRepository;
@@ -26,6 +23,7 @@ public class CompletudeController {
   }
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public Completude criarCompletude(@RequestBody Completude completude) {
     return repository.save(completude);
   }
